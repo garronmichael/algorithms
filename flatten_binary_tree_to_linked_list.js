2929c842eb2c3391d0e9e0fcdbb4687fe54db324
Garron Sanchez <garron.michael@gmail.com>
2024-08-11 12:57:35 -0700

    add flatten

---
+++ flatten_binary_tree_to_linked_list.js
@@ -0,0 +1,70 @@
+/*
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+ 
+
+Example 1:
+https://assets.leetcode.com/uploads/2021/01/14/flaten.jpg
+
+
+Input: root = [1,2,5,3,4,null,6]
+Output: [1,null,2,null,3,null,4,null,5,null,6]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+
+    flat(root)
+}
+
+var flat = function(node) {
+
+    if (node === null) {
+        return null
+    }
+
+    if (node.left === null && node.right === null) {
+        return node
+    }
+
+    let leftTail = flat(node.left)
+    let rightTail = flat(node.right)
+    if (leftTail !== null) {
+        leftTail.right = node.right
+        node.right = node.left
+        node.left = null
+    }
+
+    return rightTail === null ? leftTail : rightTail
+
+};
