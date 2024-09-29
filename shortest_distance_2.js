1f91d6349339afb2524c308cd192536fa50943d7
Garron Sanchez <garron.michael@gmail.com>
2024-09-29 10:36:08 -0700

    solve shortestDistance2

---
+++ shortest_distance_2.js
@@ -0,0 +1,66 @@
+/*
+Design a data structure that will be initialized with a string array, and then it should answer queries of the shortest distance between two different strings from the array.
+
+Implement the WordDistance class:
+
+WordDistance(String[] wordsDict) initializes the object with the strings array wordsDict.
+int shortest(String word1, String word2) returns the shortest distance between word1 and word2 in the array wordsDict.
+ 
+
+Example 1:
+
+Input
+["WordDistance", "shortest", "shortest"]
+[[["practice", "makes", "perfect", "coding", "makes"]], ["coding", "practice"], ["makes", "coding"]]
+Output
+[null, 3, 1]
+
+Explanation
+WordDistance wordDistance = new WordDistance(["practice", "makes", "perfect", "coding", "makes"]);
+wordDistance.shortest("coding", "practice"); // return 3
+wordDistance.shortest("makes", "coding");    // return 1
+ 
+
+Constraints:
+
+1 <= wordsDict.length <= 3 * 104
+1 <= wordsDict[i].length <= 10
+wordsDict[i] consists of lowercase English letters.
+word1 and word2 are in wordsDict.
+word1 != word2
+At most 5000 calls will be made to shortest.
+*/
+
+/**
+ * @param {string[]} wordsDict
+ */
+var WordDistance = function(wordsDict) {
+    this.list = wordsDict
+};
+
+/** 
+ * @param {string} word1 
+ * @param {string} word2
+ * @return {number}
+ */
+WordDistance.prototype.shortest = function(word1, word2) {
+    let idx1 = -1
+    let idx2 = -1
+    let min = this.list.length
+
+    for (let i = 0; i < this.list.length; i++) {
+        if (this.list[i] === word1) idx1 = i
+        if (this.list[i] === word2) idx2 = i
+        if(idx1 > -1 && idx2 > -1) {
+            min = Math.min(min, Math.abs(idx1 - idx2))
+        }
+    }
+
+    return min
+};
+
+/** 
+ * Your WordDistance object will be instantiated and called as such:
+ * var obj = new WordDistance(wordsDict)
+ * var param_1 = obj.shortest(word1,word2)
+ */
