e89fd79313ec6d45ec786722f225c5c9acd06062
Garron Sanchez <garron.michael@gmail.com>
2024-11-18 11:51:56 -0800

    complete k nearest neightbors

---
+++ grokking_algorithms/k_nearest_neighbors.py
@@ -0,0 +1,6 @@
+# 12.1 Try to normalize that data set with some multipliers so that they have equivalent averages and can be compared on the same scale.
+# Otherwise you could have limited to rating system to like or didn't like instead of multiple options.
+
+# 12.2 Give more weight to the influences scores by adding additional scores of the influencer in the equations. 
+
+# 12.3 You should look at sqrt(n) neighbors for n number of users/items/etc.
